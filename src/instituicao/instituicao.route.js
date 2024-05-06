705fa06ba2cb78db2d924c341d1a7436d5ddbed4
eletConnect <164717570+eletConnect@users.noreply.github.com>
2024-05-06 12:21:16 -0300

    Delete src directory

--- src/instituicao/instituicao.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const escolaController = require("./instituicao.controller");
-
-router.post('/', escolaController.verificarEscola);
-router.post('/cadastrar', escolaController.cadastrarInstituicao);
-
-module.exports = router;
--- src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require("../api/controllers/authController");
-
-// Rota para login
-router.post('/login', authController.login);
-router.post('/register', authController.register);
-
-module.exports = router;
