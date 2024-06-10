3186aceb130fe5c3c2cb5d1892a53e7fca46d4ab
eletConnect <164717570+eletConnect@users.noreply.github.com>
2024-06-10 10:33:46 -0300

    Delete src directory

--- src/aluno/aluno.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require("./aluno.controller");
-
-router.get('/', authController.listarAlunos);
-
-
-module.exports = router;
